980c76ef2a0c379d5d837c88f5129ddf6d3c4ef2
jsmentch <jsmentch@gmail.com>
2021-03-03 18:13:34 -0500

    add loading mesh to convenience fn for hcp_tools

--- code/hcp_tools.py
+++ code/hcp_tools.py
@@ -1,3 +1,7 @@
+# convenience functions for working with hcp data
+#
+# specifically extend hcp_utils to work with 7T data 
+
 import nibabel as nb
 import hcp_utils as hcp
 
@@ -7,3 +11,7 @@ def extract_cortex(image_file):
     vertex_info = hcp.get_HCP_vertex_info(img)
     img_fdata_cortex_data = hcp.cortex_data(img_fdata[0], fill=0, vertex_info=vertex_info)
     return img_fdata_cortex_data
+
+def load_meshes(example_filename):
+    mesh59k = hcp.load_surfaces(example_filename=example_filename)
+    return mesh59k
