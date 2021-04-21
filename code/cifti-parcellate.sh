59876484de0c691740476ef7595664752405a067
jsmentch <jsmentch@gmail.com>
2021-04-21 16:18:42 -0400

    parcellate cifti

---
+++ code/cifti-parcellate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# run wb_command
+#inputs: subject number, movie title
+#usage: ./cifti-parcellate.sh 100610 tfMRI_MOVIE1_7T_AP
+
+echo "running wb_command -cifti-parcellate"
+echo "subject = " $1
+echo "movie = " $2
+
+wb_command -cifti-parcellate "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$1/MNINonLinear/Results/$2/$2_Atlas_1.6mm_MSMAll_hp2000_clean.dtseries.nii" "../sourcedata/data/parcellations/Q1-Q6_RelatedParcellation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.59k_fs_LR.dlabel.nii" COLUMN "sub$1_$2.ptseries.nii"
--- code/test.sh
+++
@@ -1,1 +0,0 @@
-echo "testing datalad containers-run"
