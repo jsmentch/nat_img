fix plotting script scaling issues

code/analysis.py
@@ -20,6 +20,9 @@ sns.set("paper", "white")
 plt.rcParams['axes.facecolor'] = 'white'
 plt.rcParams['figure.facecolor'] = 'white'
 
+# rcParams = np.load('rcParams.npy', allow_pickle=True)
+# plt.rcParams = rcParams
+
 def load_data_HCP(subject,feature,n_movies):
     # Inputs: subject = HCP id eg 100610
     #         feature='mfs'
@@ -179,203 +182,6 @@ def simple_ridgeCV(X,Y):
     weights_mean = np.mean(weights, axis=0)
     return scores_mean,corr_mean,weights_mean
 
-def plot_59k_results(scores,score_type,vertex_info,subject,feature,dataset,title):
-    if score_type == 'r2':
-        v=[0,0.5]
-        threshold=None
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'r':
-        v=[0,1]
-        threshold=None
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'p':
-        v=[0,0.05]
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'z':
-        v=[-10,10]
-        threshold=3
-        symmetric_cmap=True
-        cmap='cold_hot'
-    if score_type == 'd':
-        v=[0,10]
-        threshold=3
-        symmetric_cmap=True
-        cmap='inferno'
-    if score_type == 'raw':
-        v=[-10,10]
-        threshold=1
-        symmetric_cmap=True
-        cmap='cold_hot'
-    if not os.path.exists(f'../outputs/figures/{dataset}/'):
-        os.mkdir(f'../outputs/figures/{dataset}/')
-    figpath=f'../outputs/figures/{dataset}/{score_type}'
-    if not os.path.exists(figpath):
-        os.mkdir(figpath)
-    save_dir=f'{figpath}/{title}{str(subject)}_{feature}'
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-    flatmeshes=load_flatmaps_59k() #load flatmaps
-    surf_path_msm = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/100610.L.inflated_1.6mm_MSMAll.59k_fs_LR.surf.gii'
-    mesh59k_msm = load_meshes(example_filename=surf_path_msm) #load other meshes
-    # get data from results in plotting format
-    score_cortex_dataL = hcp.left_cortex_data(scores, fill=0, vertex_info=vertex_info)
-    score_cortex_dataR = hcp.right_cortex_data(scores, fill=0, vertex_info=vertex_info)
-    # sulcal depth paths
-    sulc_left = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/MNINonLinear/fsaverage_LR59k/100610.L.sulc.59k_fs_LR.shape.gii'
-    sulc_right = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/MNINonLinear/fsaverage_LR59k/100610.R.sulc.59k_fs_LR.shape.gii'
-    # params for view to plot
-    params = [('flat_L',score_cortex_dataL,flatmeshes.flat_left,sulc_left,'left'),\
-     ('flat_R',score_cortex_dataR,flatmeshes.flat_right,sulc_right,'right'),\
-     ('vinf_L',score_cortex_dataL,mesh59k_msm.very_inflated_left,sulc_left,'left'),\
-     ('vinf_R',score_cortex_dataR,mesh59k_msm.very_inflated_right,sulc_right,'right'),\
-    ]
-    # plot each hemi and mesh, save to outputs dir
-    for name, data, mesh, sulc, hemi in params:
-        plot_surf(mesh,\
-                data, \
-                  cmap=cmap,symmetric_cmap=symmetric_cmap, avg_method='median',#figure=fig,\
-                bg_map=sulc, colorbar=True, vmin=v[0], vmax=v[1], threshold=threshold, hemi=hemi, \
-                data_alpha=np.where(data>0,1,0),\
-                data_remove=np.zeros(data.shape),output_file=f'{save_dir}/{name}.png')
-
-    #combine saved maps into one with PIL
-    area = (75, 140, 635, 560) #area to crop from each image
-
-    img = Image.open(f'{save_dir}/flat_L.png')
-    cropped = img.crop(area)
-    fL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
-    w,h = img.size
-    c_area = (690, 0, w-10, h) # area of colorbar to crop
-    cbar = img.crop(c_area)
-
-    img = Image.open(f'{save_dir}/flat_R.png')
-    fR = img.crop(area)
-
-    img = Image.open(f'{save_dir}/vinf_L.png')
-    iL = img.crop(area)
-    #iL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
-
-    img = Image.open(f'{save_dir}/vinf_R.png')
-    iR = img.crop(area)
-
-    w,h=iR.size
-
-    new_im = Image.new('RGB', ( (w*2)+70 , h*2) ,(255, 255, 255, 1))
-    new_im.paste(fL,(0,h))
-    new_im.paste(fR,(w,h))
-    new_im.paste(iL,(0,0))
-    new_im.paste(iR,(w,0))
-    new_im.paste(cbar,(w*2,int(round(h/4))))
-
-    w,h=new_im.size
-
-    draw = ImageDraw.Draw(new_im)
-    draw.text((0,0),f"{title}_{subject}_{feature}_{score_type}",(0,0,0))
-
-    new_im.save(f'{save_dir}/../{title}_{subject}_{feature}.png')
-    
-    
-def plot_32k_results(scores,score_type,subject,feature,dataset,title):
-    if score_type == 'r2':
-        v=[0,0.5]
-        threshold=None
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'r':
-        v=[0,1]
-        threshold=None
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'p':
-        v=[0,0.05]
-        symmetric_cmap=False
-        cmap='inferno'
-    if score_type == 'z':
-        v=[-10,10]
-        threshold=3
-        symmetric_cmap=True
-        cmap='cold_hot'
-    if score_type == 'd':
-        v=[0,10]
-        threshold=3
-        symmetric_cmap=True
-        cmap='inferno'
-    if score_type == 'raw':
-        v=[-10,10]
-        threshold=1
-        symmetric_cmap=True
-        cmap='cold_hot'
-    if not os.path.exists(f'../outputs/figures/{dataset}/'):
-        os.mkdir(f'../outputs/figures/{dataset}/')
-    figpath=f'../outputs/figures/{dataset}/{score_type}'
-    if not os.path.exists(figpath):
-        os.mkdir(figpath)
-    save_dir=f'{figpath}/{title}{str(subject)}_{feature}'
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-#     flatmeshes=load_flatmaps_59k() #load flatmaps
-#     surf_path_msm = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/100610.L.inflated_1.6mm_MSMAll.59k_fs_LR.surf.gii'
-#     mesh59k_msm = load_meshes(example_filename=surf_path_msm) #load other meshes
-    # get data from results in plotting format
-    score_cortex_dataL = hcp.left_cortex_data(scores, fill=0)
-    score_cortex_dataR = hcp.right_cortex_data(scores, fill=0)
-#     # sulcal depth paths
-#     sulc_left = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/MNINonLinear/fsaverage_LR59k/100610.L.sulc.59k_fs_LR.shape.gii'
-#     sulc_right = '../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/MNINonLinear/fsaverage_LR59k/100610.R.sulc.59k_fs_LR.shape.gii'
-#     # params for view to plot
-    params = [('flat_L',score_cortex_dataL,hcp.mesh.flat_left,hcp.mesh.sulc_left,'left'),\
-     ('flat_R',score_cortex_dataR,hcp.mesh.flat_right,hcp.mesh.sulc_right,'right'),\
-     ('vinf_L',score_cortex_dataL,hcp.mesh.very_inflated_left,hcp.mesh.sulc_left,'left'),\
-     ('vinf_R',score_cortex_dataR,hcp.mesh.very_inflated_right,hcp.mesh.sulc_right,'right'),\
-    ]
-    # plot each hemi and mesh, save to outputs dir
-    for name, data, mesh, sulc, hemi in params:
-        plot_surf(mesh,\
-                data, \
-                  cmap=cmap,symmetric_cmap=symmetric_cmap, avg_method='median',#figure=fig,\
-                bg_map=sulc, colorbar=True, vmin=v[0], vmax=v[1], threshold=threshold, hemi=hemi, \
-                data_alpha=np.where(data>0,1,0),\
-                data_remove=np.zeros(data.shape),output_file=f'{save_dir}/{name}.png')
-
-    #combine saved maps into one with PIL
-    area = (75, 140, 635, 560) #area to crop from each image
-
-    img = Image.open(f'{save_dir}/flat_L.png')
-    cropped = img.crop(area)
-    fL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
-    w,h = img.size
-    c_area = (690, 0, w-10, h) # area of colorbar to crop
-    cbar = img.crop(c_area)
-
-    img = Image.open(f'{save_dir}/flat_R.png')
-    fR = img.crop(area)
-
-    img = Image.open(f'{save_dir}/vinf_L.png')
-    iL = img.crop(area)
-    #iL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
-
-    img = Image.open(f'{save_dir}/vinf_R.png')
-    iR = img.crop(area)
-
-    w,h=iR.size
-
-    new_im = Image.new('RGB', ( (w*2)+70 , h*2) ,(255, 255, 255, 1))
-    new_im.paste(fL,(0,h))
-    new_im.paste(fR,(w,h))
-    new_im.paste(iL,(0,0))
-    new_im.paste(iR,(w,0))
-    new_im.paste(cbar,(w*2,int(round(h/4))))
-
-    w,h=new_im.size
-
-    draw = ImageDraw.Draw(new_im)
-    draw.text((0,0),f"{title}_{subject}_{feature}_{score_type}",(0,0,0))
-
-    new_im.save(f'{save_dir}/../{title}_{subject}_{feature}_{score_type}.png')
-    
 def plot_results(scores,score_type,data_type,vertex_info,subject,feature,dataset,title):
     '''Inputs:
         scores = data to plot
@@ -452,6 +258,7 @@ def plot_results(scores,score_type,data_type,vertex_info,subject,feature,dataset
         ]
     # plot each hemi and mesh, save to outputs dir
     for name, data, mesh, sulc, hemi in params:
+        #figure, axes = plt.subplots(subplot_kw=dict(projection="3d"), figsize=(6,4))
         plot_surf(mesh,\
                 data, \
                   cmap=cmap,symmetric_cmap=symmetric_cmap, avg_method='median',#figure=fig,\
@@ -459,23 +266,30 @@ def plot_results(scores,score_type,data_type,vertex_info,subject,feature,dataset
                 data_alpha=np.where(data>0,1,0),\
                 data_remove=np.zeros(data.shape),output_file=f'{scratch_dir}/{name}.png')
 #combine saved maps into one with PIL
+#     if notebook==True:
     area = (75, 140, 635, 560) #area to crop from each image
-
-    img = Image.open(f'{scratch_dir}/flat_L.png')
+#     else:
+#         area = (105, 190, 880, 780)
+        
+    img = Image.open(f'{scratch_dir}/flat_L.png',mode='r')
+    img = img.resize((770,720))
     cropped = img.crop(area)
     fL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
     w,h = img.size
     c_area = (690, 0, w-10, h) # area of colorbar to crop
     cbar = img.crop(c_area)
 
-    img = Image.open(f'{scratch_dir}/flat_R.png')
+    img = Image.open(f'{scratch_dir}/flat_R.png',mode='r')
+    img = img.resize((770,720))
     fR = img.crop(area)
 
-    img = Image.open(f'{scratch_dir}/vinf_L.png')
+    img = Image.open(f'{scratch_dir}/vinf_L.png',mode='r')
+    img = img.resize((770,720))
     iL = img.crop(area)
     #iL=cropped.transpose(Image.FLIP_LEFT_RIGHT)
 
-    img = Image.open(f'{scratch_dir}/vinf_R.png')
+    img = Image.open(f'{scratch_dir}/vinf_R.png',mode='r')
+    img = img.resize((770,720))
     iR = img.crop(area)
 
     w,h=iR.size