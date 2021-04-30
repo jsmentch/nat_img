f2443aa6e6dac49847b462e17de6e41652515382
jsmentch <jsmentch@gmail.com>
2021-04-30 14:32:04 -0400

    clean dataset

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -9,7 +9,7 @@ echo "feature = " $2
 
 datalad containers-run -m "run ridgecv subject $1" \
 --container-name analysis \
--i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/{str(subject)}/MNINonLinear/Results/*" \
+-i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$1/MNINonLinear/Results/*/*" \
 -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
 -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
 -o "../outputs/figures/HCP_7T/ridgeCV_$1_$2_r2.png" \
