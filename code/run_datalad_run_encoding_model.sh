f527adfa1efeba7c7aea9a85741e834b50c14f9a
jsmentch <jsmentch@gmail.com>
2021-04-30 15:26:51 -0400

    clean dataset

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -13,4 +13,5 @@ datalad containers-run -m "run ridgecv subject $1" \
 -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
 -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
 -o "../outputs/figures/HCP_7T/ridgeCV_$1_$2_r2.png" \
+-o "../../tmp/*" \
 "python ./run_encoding_model.py $1 $2"
