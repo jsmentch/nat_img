97273f737eea3b7b06dfcbd8029e0a81df4d1b3d
jsmentch <jsmentch@gmail.com>
2021-04-30 17:40:12 -0400

    clean dataset

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -7,11 +7,13 @@ echo "running datalad containers-run on the encoding model python script"
 echo "subject = " $1
 echo "feature = " $2
 
-datalad containers-run -m "run ridgecv subject $1" \
---container-name analysis \
--i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$1/MNINonLinear/Results/*/*" \
--i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
--i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
--o "../outputs/figures/HCP_7T/ridgeCV_$1_$2_r2.png" \
--o "../tmp/*" \
-"python ./run_encoding_model.py $1 $2"
+while IFS= read -r sub; do
+    datalad containers-run -m "run ridgecv subject $sub" \
+    --container-name analysis \
+    -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
+    -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
+    -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
+    -o "../outputs/figures/HCP_7T/ridgeCV_$sub_$2_r2.png" \
+    -o "../tmp/*" \
+    "python ./run_encoding_model.py $sub $2"
+done < "subjects.txt"
