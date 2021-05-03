4b69b8a2a191e1bfab421f08fe79bc75650358ea
jsmentch <jsmentch@gmail.com>
2021-05-03 15:20:30 -0400

    add merlin 3T to encoding model scripts

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
-# given an HCP 7t subject id and feature string, run datalad containers-run the encoding model python script
+# given a subject id, feature, and dataset, run datalad containers-run the encoding model python script
 #inputs: subject id (str), feature name (str)
-#usage: ./run_datalad_run_encoding_model.sh 100610 as_scores
+#usage: ./run_datalad_run_encoding_model.sh 100610 as_scores HCP_7T
 
 echo "running datalad containers-run on the encoding model python script"
 echo "subject = " $1
 echo "feature = " $2
+echo "dataset = " $3
 
 while IFS= read -r sub; do
-    FILE="../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png"
+    FILE="../outputs/figures/$3/ridgeCV_${sub}_$2_r2.png"
     if test -e "$FILE"; then
         echo "$FILE exists, skipping."
     else
         echo "$FILE does not exist."
         echo "running ${sub} $2"
-        datalad containers-run -m "run ridgecv subject $sub" \
-        --container-name analysis \
-        -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
-        -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
-        -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
-        -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
-        -o "../tmp/*" \
-        "python ./run_encoding_model.py $sub $2"
+        
+        if $3 == 'HCP_7T'
+            datalad containers-run -m "run $3 ridgecv subject $sub" \
+            --container-name analysis \
+            -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
+            -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
+            -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
+            -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
+            -o "../tmp/*" \
+            "python ./run_encoding_model.py $sub $2 $3"
+        elif $3 == 'merlin'
+            datalad containers-run -m "run $3 ridgecv subject $sub" \
+            --container-name analysis \
+            -i "../sourcedata/data/$3/brain/merlin_cifti_clean_smooth/${sub}_clean_smooth_task-MerlinMovie_space-fsLR_den-91k_bold.dtseries.nii" \
+            -i "../sourcedata/data/merlin/features/" \
+            -o "../outputs/figures/$3/ridgeCV_${sub}_$2_r2.png" \
+            -o "../tmp/*" \
+            "python ./run_encoding_model.py $sub $2 $3"
+        fi
         datalad save -m 'clean dataset after run'
     fi
-done < "subjects.txt"
+done < "$3_subjects.txt"
 
--- code/run_encoding_model_merlin.py
+++ code/run_encoding_model_merlin.py
@@ -10,23 +10,33 @@ import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("subject", type=str, help="input subkect id file")
+    parser.add_argument("subject", type=str, help="input subject id file")
     parser.add_argument("feature", type=str, help="which feature to use")
+    parser.add_argument("dataset", type=str, help="which dataset to use")
     args = parser.parse_args()
     subject = args.subject
     feature = args.feature
+    dataset = args.dataset
     #temp_dir = '../../tmp'
     #if not os.path.exists(temp_dir):
     #    os.mkdir(temp_dir)
+    if dataset == 'HCP_7T':
+        n_movies=[1,2,3,4]
+        X,Y,vertex_info = analysis.load_data_HCP(subject,feature,n_movies)
+        X = hrf_tools.apply_optimal_hrf_10hz(X,1)
+        scores_mean,corr_mean,weights_mean = analysis.simple_ridgeCV(X,Y)
+        #np.save(f'{temp_dir}/r2_{subject}_{feature}.npy',scores_mean)
+        #np.save(f'{temp_dir}/r_{subject}_{feature}.npy',corr_mean)
+        #np.save(f'{temp_dir}/w_{subject}_{feature}.npy',weights_mean)
+        analysis.plot_results(scores_mean,'r2','59k',vertex_info,subject,'as_scores',dataset,'ridgeCV')
+    elif dataset == 'merlin':
+        X,Y,vertex_info = analysis.load_data_merlin(subject,feature)
+        X = hrf_tools.apply_optimal_hrf_10hz(X,(1/1.5))
+        scores_mean,corr_mean,weights_mean = analysis.simple_ridgeCV(X,Y)
+        #np.save(f'{temp_dir}/r2_{subject}_{feature}.npy',scores_mean)
+        #np.save(f'{temp_dir}/r_{subject}_{feature}.npy',corr_mean)
+        #np.save(f'{temp_dir}/w_{subject}_{feature}.npy',weights_mean)
+        analysis.plot_results(scores_mean,'r2','32k',vertex_info,subject,'as_scores',dataset,'ridgeCV')
         
-    n_movies=[1,2,3,4]
-    X,Y,vertex_info = analysis.load_data_HCP(subject,feature,n_movies)
-    X = hrf_tools.apply_optimal_hrf_10hz(X,1)
-    scores_mean,corr_mean,weights_mean = analysis.simple_ridgeCV(X,Y)
-    #np.save(f'{temp_dir}/r2_{subject}_{feature}.npy',scores_mean)
-    #np.save(f'{temp_dir}/r_{subject}_{feature}.npy',corr_mean)
-    #np.save(f'{temp_dir}/w_{subject}_{feature}.npy',weights_mean)
-    analysis.plot_results(scores_mean,'r2','59k',vertex_info,subject,'as_scores','HCP_7T','ridgeCV')
-
 if __name__ == "__main__":
     main()
