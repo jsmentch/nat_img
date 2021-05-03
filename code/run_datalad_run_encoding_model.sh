70deded9e2134ee66b89a88404ca411b41a80727
jsmentch <jsmentch@gmail.com>
2021-05-02 21:27:43 -0400

    clean dataset, file exist check working

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -8,21 +8,21 @@ echo "subject = " $1
 echo "feature = " $2
 
 while IFS= read -r sub; do
-    FILE='../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png'
+    FILE="../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png"
     if test -e "$FILE"; then
         echo "$FILE exists, skipping."
     else
         echo "$FILE does not exist, skipping."
         echo "running ${sub} $2"
-#         datalad containers-run -m "run ridgecv subject $sub" \
-#         --container-name analysis \
-#         -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
-#         -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
-#         -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
-#         -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
-#         -o "../tmp/*" \
-#         "python ./run_encoding_model.py $sub $2"
-#         datalad save -m 'clean dataset after run'
+        datalad containers-run -m "run ridgecv subject $sub" \
+        --container-name analysis \
+        -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
+        -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
+        -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
+        -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
+        -o "../tmp/*" \
+        "python ./run_encoding_model.py $sub $2"
+        datalad save -m 'clean dataset after run'
     fi
 done < "subjects.txt"
 
