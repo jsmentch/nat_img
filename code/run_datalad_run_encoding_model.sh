773ad8818cb479ea84003f2c5b8519d8c02d3065
jsmentch <jsmentch@gmail.com>
2021-05-02 21:02:21 -0400

    modify datalad run bash script to skip already generated imgs

--- code/run_datalad_run_encoding_model.sh
+++ code/run_datalad_run_encoding_model.sh
@@ -8,14 +8,19 @@ echo "subject = " $1
 echo "feature = " $2
 
 while IFS= read -r sub; do
-    datalad containers-run -m "run ridgecv subject $sub" \
-    --container-name analysis \
-    -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
-    -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
-    -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
-    -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
-    -o "../tmp/*" \
-    "python ./run_encoding_model.py $sub $2"
-    datalad save -m 'clean dataset after run'
+    FILE=../outputs/figures/HCP_7T/RidgeCV_${sub}_$2_r2.png
+    if test -f "$FILE"; then
+        echo "$FILE exists, skipping."
+
+        datalad containers-run -m "run ridgecv subject $sub" \
+        --container-name analysis \
+        -i "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$sub/MNINonLinear/Results/*/*" \
+        -i "../sourcedata/data/HCP_7T_movie_FIX/features/" \
+        -i "../sourcedata/data/human-connectome-project-openaccess/HCP1200/100610/T1w/fsaverage_LR59k/*" \
+        -o "../outputs/figures/HCP_7T/ridgeCV_${sub}_$2_r2.png" \
+        -o "../tmp/*" \
+        "python ./run_encoding_model.py $sub $2"
+        datalad save -m 'clean dataset after run'
+    fi
 done < "subjects.txt"
 
