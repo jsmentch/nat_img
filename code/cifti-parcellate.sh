0952b156436ba5fe88bcccc8a2f881b96e1ecbc4
jsmentch <jsmentch@gmail.com>
2021-12-16 15:18:48 -0500

    get 59k parcellations and get auditory regions

--- code/cifti-parcellate.sh
+++ code/cifti-parcellate.sh
@@ -13,13 +13,4 @@ datalad containers-run -m "run ridgecv subject $1" \
 -i "sourcedata/data/HCP_7T_movie_FIX/features/*.npy" -o "sourcedata/data/HCP_7T_movie_FIX/features_hrf/*" "python code/hrf_tools.py sourcedata/data/HCP_7T_movie_FIX/features/ sourcedata/data/HCP_7T_movie_FIX/features_hrf -d"
 
 
-
-
-
-
-
-
-
-
-
 wb_command -cifti-parcellate "../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$1/MNINonLinear/Results/$2/$2_Atlas_1.6mm_MSMAll_hp2000_clean.dtseries.nii" "../sourcedata/data/parcellations/Q1-Q6_RelatedParcellation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.59k_fs_LR.dlabel.nii" COLUMN "sub$1_$2.ptseries.nii"
