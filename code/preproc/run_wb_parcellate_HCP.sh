4ee76c8a363bc22685ba1c1c6df54486aa858545
jsmentch <jsmentch@gmail.com>
2022-03-25 18:00:06 -0400

    MMP parcellation of HCP 7T movies

--- code/preproc/run_wb_parcellate_HCP.sh
+++ code/preproc/run_wb_parcellate_HCP.sh
@@ -14,7 +14,7 @@ do
 	par_dir=../../sourcedata/data/HCP_7T_movie_FIX/brain/parcellations/parcellated/
 	mkdir -p $par_dir$s
 	wb_command -cifti-parcellate \
-	    ../../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$s/MNINonLinear/Results/$m/$m_Atlas_1.6mm_MSMAll_hp2000_clean.dtseries.nii \
+	    ../../sourcedata/data/HCP_7T_movie_FIX/brain/HCP_7T_movie_FIX/$s/MNINonLinear/Results/${m}/${m}_Atlas_1.6mm_MSMAll_hp2000_clean.dtseries.nii \
 	    ../../sourcedata/data/parcellations/Q1-Q6_RelatedParcellation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.59k_fs_LR.dlabel.nii \
 	    COLUMN \
 	    $par_dir$s/sub${s}_${m}.ptseries.nii
